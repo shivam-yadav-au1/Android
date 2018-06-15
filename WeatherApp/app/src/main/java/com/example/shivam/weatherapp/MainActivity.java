c72be1c093b6561b881238c54fbffbd4bc3d87b1
shivam <shivamcs0049@gmail.com>
2018-06-15 13:02:10 +0530

    wether app

---
+++ WeatherApp/app/src/main/java/com/example/shivam/weatherapp/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.shivam.weatherapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_main);
+
+        // Spinner element
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_toolBar);
+
+        // Spinner click listener
+       /* spinner.setOnItemSelectedListener(this);*/
+
+        // Spinner Drop down elements
+        List<String> categories = new ArrayList<String>();
+        categories.add("Automobile");
+        categories.add("Business Services");
+        categories.add("Computers");
+        categories.add("Education");
+        categories.add("Personal");
+        categories.add("Travel");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+    }
+}
